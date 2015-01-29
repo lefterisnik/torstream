98fe700bc2cd6763ab3704aefe5974e59389949a
lefterisnik <lefteris.nikoltsios@gmail.com>
2015-01-30 01:30:48 +0200

    Change file structure.
    1) Change file structure.
    2) Add remove torrent function.
    3) Add open folder of torrent storage function.
    4) Fix vlc opening.

---
+++ torstream/about.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from gi.repository import Gtk
+
+class About:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("ui/about.ui")
+        self.dialog = self.builder.get_object("aboutdialog")
+        self.dialog.run()
+        self.dialog.destroy()
---
+++ torstream/common/dialogs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from gi.repository import Gtk
+
+
+class Chooser(Gtk.FileChooserDialog):
+    def __init__(self, title, parent, action = None, buttons = None):
+        super(Chooser, self).__init__(title = title,
+                                      parent = parent)
+        if action:
+            self.set_action(action)
+        else:
+            self.set_action(Gtk.FileChooserAction.OPEN)
+    
+        if buttons:
+            self.add_buttons(buttons)
+        else:
+            self.add_buttons(Gtk.STOCK_CANCEL,
+                             Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OPEN,
+                             Gtk.ResponseType.OK)
+            
+        self.add_filters(self)
+        self.set_default_response(Gtk.ResponseType.CANCEL)
+
+    def add_filters(self, dialog):
+        '''
+        Add filter for torrent files to choose dialog
+        :param dialog:
+        :return: None
+        '''
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Torrent files")
+        filter_text.add_mime_type("application/x-bittorrent")
+        filter_text.add_pattern("*.torrent")
+        dialog.add_filter(filter_text)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+    
+    def showup(self):
+        '''
+        main function to run choose dialog and return filename
+        :return: choosen filename
+        '''
+        filename = None
+        response = self.run()
+        if response == Gtk.ResponseType.OK:
+            filename = self.get_filename()
+        self.destroy()
+        return filename
+
+
+class RemoveTorrent(Gtk.MessageDialog):
+    def __init__(self, message_format):
+        super(RemoveTorrent, self).__init__(flags = Gtk.DialogFlags.MODAL,
+                                            message_type = Gtk.MessageType.QUESTION,
+                                            buttons = Gtk.ButtonsType.OK_CANCEL,
+                                            message_format = message_format)
+
+
+    def showup(self):
+        '''
+        main function to run remove torrent dialog
+        :return: response
+        '''
+        remove = None
+        response = self.run()
+        if response == Gtk.ResponseType.OK:
+            remove = True
+        else:
+            remove = False
+        self.destroy()
+        return remove
